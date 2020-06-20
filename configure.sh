62e9b4d5203b57817dd64e47ac7dc9862527be3f
pyrad <cailong@163.com>
2020-06-20 21:32:53 +0800

    modify configure.sh

--- configure.sh
+++ configure.sh
@@ -3,9 +3,22 @@
 if [[ -z $BOOST_INC_HOME ]]; then
     echo "Env var BOOST_INC_HOME is not found"
     exit
+else
+    echo "Found env var BOOST_INC_HOME"
 fi
 
 if [[ -z $BOOST_LIB_HOME ]]; then
     echo "Env var BOOST_LIB_HOME is not found"
     exit
+else
+    echo "Found env var BOOST_LIB_HOME"
 fi
+
+MKF_TMPL="./template.makefile"
+TAR_MAKEFILE="GNUmakefile"
+if [[ -f $MKF_TMPL  ]]; then
+    echo "Template file for Makefile is not found"
+    exit
+fi
+
+cp $MKF_TMPL $TAR_MAKEFILE
