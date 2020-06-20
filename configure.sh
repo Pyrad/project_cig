c1d50ae792bd92b3a3aaa85893763d8e7d2c0c94
pyrad <cailong@163.com>
2020-06-20 21:36:22 +0800

    fix a logic bug

--- configure.sh
+++ configure.sh
@@ -4,20 +4,20 @@ if [[ -z $BOOST_INC_HOME ]]; then
     echo "Env var BOOST_INC_HOME is not found"
     exit
 else
-    echo "Found env var BOOST_INC_HOME"
+    echo "Found env var BOOST_INC_HOME: $BOOST_INC_HOME"
 fi
 
 if [[ -z $BOOST_LIB_HOME ]]; then
     echo "Env var BOOST_LIB_HOME is not found"
     exit
 else
-    echo "Found env var BOOST_LIB_HOME"
+    echo "Found env var BOOST_LIB_HOME: $BOOST_LIB_HOME"
 fi
 
 MKF_TMPL="./template.makefile"
 TAR_MAKEFILE="GNUmakefile"
-if [[ -f $MKF_TMPL  ]]; then
-    echo "Template file for Makefile is not found"
+if [[ ! -f $MKF_TMPL ]]; then
+    echo "Template file($MKF_TMPL) for Makefile is not found"
     exit
 fi
 
