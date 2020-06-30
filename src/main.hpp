dc997bde86c222c4a3322deb5fc09f465702b3cf
pyrad <cailong89@163.com>
2020-06-30 14:48:02 +0800

    Add header file for main.cpp

---
+++ src/main.hpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : main.hpp
+// Author      : Pyrad
+// Version     :
+// Copyright   : Pyrad
+// Description : Project for Code Interview Guide
+//============================================================================
+
+#include <iostream>
+
+void print_global_info() {
+    std::cout << "This is the main header file" << std::endl;
+
+    return;
+}
