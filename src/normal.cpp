6c9f1dafcffd0deb7656c40cf39a9c2a7c27c1bd
pyrad <cailong89@163.com>
2020-07-11 20:53:33 +0800

    Remove comment code

--- src/normal.cpp
+++ src/normal.cpp
@@ -6,51 +6,6 @@ namespace NORMAL {
 
 namespace CU = common_utils;
 
-#if 0
-CU::node* create_random_binary_tree(int height, int minlimit, int maxlimit, bool full) {
-    using node = CU::node;
-
-    if (height <= 0) {
-        return nullptr;
-    }
-
-    int node_num = pow(2, height) - 1;
-
-    node **p = new node*[node_num];
-    node *head = new node(CU::gen_random_int_1(minlimit, maxlimit));
-    p[0] = head;
-
-    for (int i = 1; i < node_num; ++i) {
-        if (!full) {
-            int s = CU::gen_random_int_1(minlimit, maxlimit);
-            if (s % 2 == 0) {
-                continue;
-            }
-        }
-
-        int parent = (i - 1) / 2;
-        node *cur = new node(CU::gen_random_int_1(minlimit, maxlimit));
-        p[i] = cur;
-
-        if (p[parent]) {
-            if( i % 2 == 0) {
-                p[parent]->right = cur;
-            } else {
-                p[parent]->left = cur;
-            }
-        }
-    }
-
-    delete [] p;
-
-    return head;
-}
-
-CU::node* create_random_binary_tree_full(int height, int minlimit, int maxlimit) {
-    return create_random_binary_tree(height, minlimit, maxlimit, true);
-}
-#endif // 0
-
 void run_all() {
 
 }
--- src/normal.hpp
+++ src/normal.hpp
@@ -6,11 +6,6 @@
 
 namespace NORMAL {
 
-#if 0
-common_utils::node* create_random_binary_tree(int height, int minlimit=0, int maxlimit=RAND_MAX, bool full=false);
-common_utils::node* create_random_binary_tree_full(int height, int minlimit=0, int maxlimit=RAND_MAX);
-#endif // 0
-
 void run_all();
 
 } // End of namespace NORMAL
