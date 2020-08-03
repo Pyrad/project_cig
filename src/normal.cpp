f4d9bbf095b5dff015f84ef0c04c16d2f6ab3b8d
pyrad <cailong89@163.com>
2020-08-03 19:53:02 +0800

    Check if temp folder exists

--- src/normal.cpp
+++ src/normal.cpp
@@ -7,6 +7,8 @@
 #include <unordered_map>
 #include <fstream>
 
+#include <boost/filesystem.hpp>
+
 namespace NORMAL {
 
 namespace CU = common_utils;
@@ -17,11 +19,19 @@ void run_all() {
 
 void test_write_binary_tree_to_graphviz() {
     using node = CU::node;
+    namespace BF = boost::filesystem;
     // node* phead = create_random_binary_tree_full(5, 0, 50);
     node* phead = CU::create_random_binary_tree(5, 0, 50);
     std::function<void(node*)> f = [](node* h) {if (h) { printf("%d ", h->value); }};
     CU::pre_order(phead, f);
     printf("\n");
+    std::string dirname("temp");
+    BF::path dpath(dirname.c_str());
+    if (BF::is_directory(dpath)) {
+        printf("%s is a directory\n", dirname.c_str());
+    } else {
+        printf("%s is not a directory or doesn't exist\n", dirname.c_str());
+    }
     std::fstream fs("./temp/graphtest.dot", std::fstream::out);
     if (!fs.is_open()) {
         printf("Error to open file for writing: ./temp/graphtest.dot");
